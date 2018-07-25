12e51a258401d00b940193fe5b11e468a7e072a4
mnakagawa <mnakagawa@MacBook-Pro-de-Mnakagawa.local>
2018-07-24 20:26:26 -0500

    added rayquaza

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res)=>{
 
 app.get('/pokemons', (req, res) => {
 
-    let pokeIds = [1, 2, 3];
+    let pokeIds = [1, 2, 3, 5, 7, 45, 130, 384];
 
     let promesasIncompletas = pokeIds.map((id)=>{
 
